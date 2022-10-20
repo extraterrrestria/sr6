4efe359833a639417a4e14054f208729c9985344
extraterrrestria <114211155+extraterrrestria@users.noreply.github.com>
2022-10-20 19:41:57 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,8 +3,8 @@ size = int(input('Введите размерность массива: '))
 arr = []
 for i in range(size):
     arr.append(float(input('Введите элементы массива: ')))
-print('Исходный массив: \n'', arr)
+print('Исходный массив: \n', arr)
 index = arr.index(max(arr))
 for j in range(index + 1, size):
     arr[j] = 0
-print('Изменённый массив: \n'', arr)
+print('Изменённый массив: \n', arr)
